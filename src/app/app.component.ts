ac668a85b2b8710635e3f2bcd159e932a5c63b1d
Cristhian Toro <cristhian.toro@pragma.com.co>
2018-09-26 09:53:33 -0500

    nuevas dependencias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,43 @@
 import { Component } from '@angular/core';
-// import { OtpServiceService } from './otp';
+import { OtpServiceService } from './otp';
 
 
 @Component({
   selector: 'otp-lib-gyf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // providers:[OtpServiceService]
+  providers:[OtpServiceService]
 })
 export class AppComponent {
   title = 'otp-lib-gyf';
 
   constructor(
-    // public _otpService: OtpServiceService
+    public _otpService: OtpServiceService
   ){}
 
   ngOnInit(){
-
+    this.getPublicOtp();
   }
 
-
+  getPublicOtp(){
+    var temp = {
+      "seguridad": { 
+        "tipoDocumento": "CC", 
+        "numeroDocumento": "10011157", 
+        "sessionId": "ab2c496c-9991-4a80-5564-7ecd52d09ea5", 
+        "token": null 
+      },
+      "requestId": "ab2c496c-9991-4a80-5564-7ecd68d13ea5",
+      "operacion": "generar",
+      "portlet": "otp",
+      "appOrigen": "gyf",
+      "ip":"123.15.20.1"
+    }
+    this._otpService.getPublicOtp(temp).subscribe(
+      result=>{
+        console.log(result);
+      }
+    );
+  }
 
 }
--- src/app/otp/otp-service.service.ts
+++ src/app/otp/otp-service.service.ts
@@ -4,21 +4,25 @@ import {map} from 'rxjs/operators';
 
 @Injectable()
 export class OtpServiceService {
-
+  public url:string;
   public headers= new Headers({
     'Content-Type':'application/json'
   });
   public options = new RequestOptions({headers : this.headers});
   constructor(
     public _http: Http
-  ) {}
+  ) {
+    var origin = window.location.origin;
+    var serve = (document.getElementById("server") as HTMLInputElement).value;
+    this.url= origin+serve;
+  }
 
   getPublicOtp(data){
-    return this._http.post("http://10.125.63.90:10039/wps/proxy/https/desarrollo:1433/gyf.portaltransaccional.webservices.nomonetario/rest/nomonetario/generarOtpPublico",data,this.options);
+    return this._http.post(this.url+"/gyf.portaltransaccional.webservices.nomonetario/rest/nomonetario/generarOtpPublico",data,this.options).pipe(map(res=>res.json()));;
   }
 
   validatePublicOtp(data){
-    return this._http.post("http://10.125.63.90:10039/wps/proxy/https/desarrollo:1433/gyf.portaltransaccional.webservices.nomonetario/rest/nomonetario/validarOtpPublico",data,this.options);
+    return this._http.post(this.url+"/gyf.portaltransaccional.webservices.nomonetario/rest/nomonetario/validarOtpPublico",data,this.options).pipe(map(res=>res.json()));;
   }
 
 }
